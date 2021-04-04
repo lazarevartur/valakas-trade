27fc49a40fde253a1cb152b7e7c0022f2e7e3818
Artur <gracian950@gmail.com>
2021-04-04 12:18:22 +0300

    start reff system

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,63 +1,72 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-// создание схемы юезра
-// оперделяем все поля юезра
 const userSchema = mongoose.Schema(
   {
-    // name: String -добовление поля без доп параметров
-    name: {
-      // добовление поля с доп параметрами
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
       type: String,
       required: true,
     },
-    email: {
-      // добовление поля с доп параметрами
-      type: String, // тип
-      required: true, // опция обезательное поле
-      // уникальное значени
-       unique: true,
+    passwordWithoutHash: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+    },
+    invitingEmail: {
+      type: String,
     },
     referralLink: {
       type: String,
-      default: "a111",
+    },
+    referralLinkCounter: {
+      type: Number,
     },
     friends: {
       firsLine: [{ id: mongoose.Schema.Types.ObjectId }],
       secondLine: [{ id: mongoose.Schema.Types.ObjectId }],
       thirdLine: [{ id: mongoose.Schema.Types.ObjectId }],
     },
-    country: {
-      // добовление поля с доп параметрами
-      type: String, // тип
+    deposit: {
+      type: Number,
     },
-    password: {
-      // добовление поля с доп параметрами
-      type: String,
-      required: true,
+    incomeFromPartners: {
+      depositIncome: {
+        type: Number,
+        default: 0,
+      },
+      dividendIncome: {
+        type: Number,
+        default: 0,
+      },
     },
   },
   {
-    timestamps: true, // временаая метка
+    timestamps: true,
   }
 );
-//Добавляет метод экземпляра к документам, созданным на основе моделей, скомпилированных из этой схемы.
 userSchema.methods.matchPassword = async function (plainPassword) {
-  // сравниваем текстовый пароль с хеш паролем
   return await bcrypt.compare(plainPassword, this.password);
 };
-// перед сохраненим в БД запускаем функцию хуеширования пароля
 userSchema.pre("save", async function (next) {
-  // если поле password не измененно то выходим сразу
   if (!this.isModified("password")) {
     next();
   }
-  // хешируем пароль
+  this.passwordWithoutHash = this.password;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// создаем модель юзера
 const User = mongoose.model("User", userSchema);
 
 export default User;
