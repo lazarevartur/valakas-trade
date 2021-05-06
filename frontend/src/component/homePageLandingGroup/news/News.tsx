429fe820169ce13c46c35583df78514fc7596bfc
Artur <gracian950@gmail.com>
2021-05-06 23:44:43 +0300

    'update

--- frontend/src/component/homePageLandingGroup/news/News.tsx
+++ frontend/src/component/homePageLandingGroup/news/News.tsx
@@ -24,7 +24,9 @@ const News: React.FC<NewsProps> = ({ className = "" }) => {
                 <Card.Img
                   variant="top"
                   height={260}
-                  src="https://s3-alpha-sig.figma.com/img/e928/6161/3df0c84d344137a4b7e905e79f1fff61?Expires=1619395200&Signature=QExjXDUhg7mBFtPLTjEwNqX0IIZyyw9Q0zITYgIow3UE6x~A6gb3nymLXAUp5-3PSuFG77fzzBxrYlUpzaPTXGBEhcPeVpkwsTJ84N-7iYtGBI792mrIoWSg9WX2FiQIEskHG~~9vISeUxvScCPDG6PEmTcIQ1DX39Cf3OhZhCXfoe7jRPKnKIWArxGeFyh0BPBhxxeSC6soflRT-OtXh7IY6KWlAXj1KJrDCnxqW6wkLu63ILPsPnmXezR0S6NmQOZm0SnQLWw4regS4zI-yY0X-MnJwEgQZ~BBbbnmTaP84NGbOnZTDD9nwk8pmxYbE2XsbB-wCiDU64aP9CCzAw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  src={
+                    "https://s3-alpha-sig.figma.com/img/e928/6161/3df0c84d344137a4b7e905e79f1fff61?Expires=1621209600&Signature=fWxpiDOEj4e9J320xEQVVmaYUDSRFkCt0uoHo-57O-sxCYtNXZLweGqugCfEQ3TQYGr1xNkz8NhuWH3rKQ1EfOsB1JSYHIM5dtjT0xBUsMJC2kSgzzxqIU1lJzFDattnOzNkUBTRJbbJZHLLwnseGAcJygNKX3EKBoBBsDa0BZBf6KmwrikAm-qadG5LaJ3Oz6tuSwP8lqVhHqYu5gufNZ5oaX95eY5MqW1kGX1uAdstuWVGGNnQcIXUtjrfblg8gknnN6TRdo7focoeMErRy3187nvU24y4WdHU378SdlpqVypFkdP9W2zCDnT7KWqNokkPoY7xnF8JW6QUHwnDkQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  }
                 />
                 <Card.Body>
                   <Card.Title>Ten books to read in June</Card.Title>
@@ -35,7 +37,9 @@ const News: React.FC<NewsProps> = ({ className = "" }) => {
                 <Card.Img
                   variant="top"
                   height={260}
-                  src="https://s3-alpha-sig.figma.com/img/0160/4ccf/2ffa22fa4aac7f6896c30786066b3fc3?Expires=1619395200&Signature=KTB13KL61h3wrudTGEX0~21d~-dmmOsgnh7u2VpdYYJyotUfDQzKN~Rj94Uvfa1BldvDFzbY52MXbfw69u0MtbgCkvhgZj9hXj7Ve6Bj1f7YGc9wpgh3~gaiUrWwO-Y-VXdwkq0F-LrIdbxwOTkTowQd32c7v1j7U-Z6Z5IADpgrBlqEJ6AA37jn8xuEAUHmS38bjPSaW0t1meNvf3zhKuYDZV9gWJqrOjpsK5Kzw8qAdtIe3kcye00AB41cGeerF~zhIuJ0ObsGuH7xixulngJYtV9GfmN8BqvH3wJVx~F8Pg2dZxGJl-giS7vLCP8zBqFT1ZXMDIWqhtTizt6MhA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  src={
+                    "https://s3-alpha-sig.figma.com/img/0160/4ccf/2ffa22fa4aac7f6896c30786066b3fc3?Expires=1621209600&Signature=dYVD6ZeB8b~qqhKXInrEgTYqUyCh6imxIdMPkyKv9T~xAKm5CKFNGpDMSDGdiZOah5xQY1miyR2huh27jss-2dOAnToGNQTDf23pw2HsuRCMsQDk9bPtbSAdPgam09Cl8lDvGKosf-9Ho99Ref1gaXtt4hOsNW8y9ApUBurmfuKjtSGVRizQ~6LAipYMMZ4Oci~o3CkLLx9RH81Xd0Jt6H-GWJm2uzAadpY6~8CKwMdv1igPWXegpvDwrpX4y8Cdqo-ON8CtpVKSlzdy6vSeBp8LcziibHmmRLD~APdBCr5p1INQZPgI66CFn4luDYAnYo7eAiDuhBk~0dLoNsd51Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  }
                 />
                 <Card.Body>
                   <Card.Title>Ten books to read in June</Card.Title>
@@ -46,7 +50,9 @@ const News: React.FC<NewsProps> = ({ className = "" }) => {
                 <Card.Img
                   variant="top"
                   height={260}
-                  src="https://s3-alpha-sig.figma.com/img/be74/94f0/8517a4947d7e5f9ccc3a3732d1147fa0?Expires=1619395200&Signature=Bo7674CArs6NYec3WmQIfavZZkbxrV4ZoGPhvMQLkFNeDQU448LdFHALgdZdsQMmqrTzQWsMAp57zHkawGArjkWojd-rexx5x32hWAOEwN7Ui5JeRdfQ2X6A4hzEfxoExOCTcCxT1SOjMVVhMScmQo5niXMkTW4FWBkO-Ji8vD5kAPUhqt3RwVA8v0~oOLj1xnmNDZQfhViiZfPtm3bHEI7fs1lN11v5xA5j~a1ZzLH8sgZb9n7wAWQF2I3Ppx2dvYddDdxxusa7cMuugfVNaLO897ZtLSDY9RvwBVhB6rDlhyfC6YjYWvOVwyTekaVWEalc4J8mYHjlpSO4IZNn6w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  src={
+                    "https://s3-alpha-sig.figma.com/img/be74/94f0/8517a4947d7e5f9ccc3a3732d1147fa0?Expires=1621209600&Signature=I9kHqWk5sS6ffD~0mewrsU4dOjJ0BabAqb4z3N6JxN50s14nRHAsqjuUicDHTGwHO2vqw7QpCyowA5~v7TC1UDj895AWi4GVzhrtpKP8~23MpDQrgjm6Ht~bVXKHOSIzx7gEJSTw~JCJ1rHg9GPpZ~2-v7R5fJR6omsbSzo9KbJOqrA9-x9ZV6b3wlg-nHJ7R189Jqi0fFo0mfw3QcYsbjF2CelFhlRZgAyfm6bT5piBbS9CK4Z08QKbeUFXCta2J5JuZ2kBRzzygJfZn53QeOFpDFOXRsPGgi-9q0QFBC2Wj0C3Js8itn2DMCu~oPqNTFbXD3LsrJ-F7KJXO7~qHA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+                  }
                 />
                 <Card.Body>
                   <Card.Title>Ten books to read in June</Card.Title>
