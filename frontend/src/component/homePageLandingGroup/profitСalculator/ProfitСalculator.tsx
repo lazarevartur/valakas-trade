545b4814294a2a627d3ffac4de36f2b25604fa25
Artur <gracian950@gmail.com>
2021-05-06 23:28:20 +0300

    update calculator, func work

--- frontend/src/component/homePageLandingGroup/profitСalculator/ProfitСalculator.tsx
+++ frontend/src/component/homePageLandingGroup/profitСalculator/ProfitСalculator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProfitCalculator.module.scss";
 import { defaultComponentProps } from "../../../types/types";
 import cn from "classnames";
@@ -13,9 +13,26 @@ import {
 
 interface ProfitCalculatorProps extends defaultComponentProps {}
 
+const programsProfit = [1, 2, 3];
+
 const ProfitCalculator: React.FC<ProfitCalculatorProps> = ({
   className = "",
 }) => {
+  const [sum, setSum] = useState(1000);
+  const [currentProgram, setCurrentProgram] = useState(programsProfit[0]);
+  const computedSum = (): number => {
+    //100/100*1
+    const res = (sum / 100) * currentProgram;
+    return +res.toFixed(2);
+  };
+  const handlerOnKey: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const lengthInput = e.target.value.split("").length;
+    if (e.target.value.match(/^\d+$/) && lengthInput <= 7) {
+      setSum(+e.target.value);
+    } else if (lengthInput === 0) {
+      setSum(0);
+    }
+  };
   return (
     <div className={cn(styles.profitCalculator, { [className]: className })}>
       <Container>
@@ -27,10 +44,16 @@ const ProfitCalculator: React.FC<ProfitCalculatorProps> = ({
         <Row>
           <Col lg={{ offset: 2, span: 4 }} className={cn(styles.collum_select)}>
             <small>Инвест план</small>
-            <Form.Control as="select" className={cn(styles.form_control)}>
-              <option>FOURTH (1% в день)</option>
-              <option>FOURTH (2% в день)</option>
-              <option>FOURTH (3% в день)</option>
+            <Form.Control
+              as="select"
+              className={cn(styles.form_control)}
+              onChange={(e) => {
+                setCurrentProgram(+e.target.value);
+              }}
+            >
+              {programsProfit.map((p) => (
+                <option value={p}>FOURTH ({p}% в день)</option>
+              ))}
             </Form.Control>
           </Col>
           <Col lg={4} className={cn(styles.collum_select)}>
@@ -41,6 +64,8 @@ const ProfitCalculator: React.FC<ProfitCalculatorProps> = ({
                 aria-label="Сумма"
                 aria-describedby="basic-addon1"
                 className={cn(styles.form_control)}
+                value={sum}
+                onChange={handlerOnKey}
               />
             </InputGroup>
           </Col>
@@ -48,12 +73,12 @@ const ProfitCalculator: React.FC<ProfitCalculatorProps> = ({
         <Row className={styles.output}>
           <Col lg={{ offset: 2, span: 4 }} className={cn(styles.collum)}>
             <h6>НАЧИСЛЕНИЕ</h6>
-            <p>50.00$</p>
+            <p>{computedSum()}$</p>
             <small>Вдень</small>
           </Col>
           <Col lg={4} className={styles.collum}>
             <h6>ОБЩАЯ ПРИБЫЛЬ</h6>
-            <p>45000.00$</p>
+            <p>{(computedSum() * 250).toFixed(2)}$</p>
             <small>За весь срок</small>
           </Col>
         </Row>
