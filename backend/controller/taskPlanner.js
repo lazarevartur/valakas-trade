8f22b3134d7c24d4543025a4b9b21725926ef43f
Artur <gracian950@gmail.com>
2021-06-10 01:00:12 +0300

    rework logic

--- backend/controller/taskPlanner.js
+++ backend/controller/taskPlanner.js
@@ -2,39 +2,57 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
 export const dailyUpdate = asyncHandler(async (req, res) => {
-    const allUsers = await User.find({}).populate({
-        path:
-            'partners.first partners.second partners.third partners.fourth partners.fifth',
-    });
+    const dailyInterest = req.body.dailyInterest || 0.02;
+    try {
+        const allUsers = await User.find({}).populate({
+            path:
+                'partners.first partners.second partners.third partners.fourth partners.fifth',
+        });
+        const userIncomeMap = {};
+        // Каждый день капает от 0.1-2% от суммы депозита ( линейный доход )
+        const stageOne = allUsers.map((user) => {
+            const income = user.wallets.start_account * dailyInterest;
+            userIncomeMap[user.email] = income;
+            user.wallets.operating_account += income;
 
-    allUsers.forEach((user) => {
-        console.log('=================================');
-        console.log('---------------------------');
-        console.log(user.wallets, 'было');
-        user.wallets.operating_account += user.wallets.start_account * 0.01;
-        console.log(user.wallets, 'стало');
-        console.log('---------------------------');
+            return user.save();
+        });
+        // Каждый день капает от 5-2% от линейного дохода партнеров ( дивиденды )
+        const stageTwo = await Promise.all(stageOne);
 
-        const partnersLines = Object.values(user.partners.toObject());
-        partnersLines.forEach((lines) => {
-            if (lines) {
-                lines.forEach((partner) => {
-                    console.log(
-                        partner.wallets.operating_account,
-                        '+++++++++++++++++++++++++++++++++++++'
-                    );
-                    console.log(user.wallets, 'БЫЛО');
-                    console.log(partner.wallets.operating_account, 'partner');
-                    user.wallets.bonus_account +=
-                        partner.wallets.operating_account * 0.01;
-                    console.log(user.wallets, 'СТАЛО');
-                });
-            }
+        stageTwo.map((user) => {
+            const partnersLines = Object.values(user.partners.toObject());
+            console.log(user.email, 'email');
+            partnersLines.forEach((lines, line) => {
+                if (lines.length) {
+                    lines.forEach((partner, i) => {
+                        console.log(
+                            partner.email,
+                            `line:${line + 1} partner:${i + 1} `
+                        );
+                        const interestRate = +(0.05 - +`0.0${line}`).toFixed(2);
+                        const partnerIncome = userIncomeMap[partner.email];
+                        user.wallets.bonus_account +=
+                            partnerIncome * interestRate;
+                    });
+
+                    console.log('--------------');
+                }
+            });
+
+            return user.save();
         });
 
-        user.save();
-        console.log('=================================');
-    });
-
-    res.send('good');
+        res.send({
+            status: 0,
+            statusCode: 'success',
+            message: 'Successfully update.',
+        });
+    } catch (error) {
+        return res.send({
+            status: 1,
+            statusCode: 'error',
+            message: error.message,
+        });
+    }
 });
